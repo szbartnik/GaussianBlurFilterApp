f00f3be8553fe5c8a7bf85ce486c2006fd61e62b
Szymon Bartnik <szymbar099@student.polsl.pl>
2014-11-02 15:02:30 +0100

    Implemented computing pascal triangle row in ASM library.

--- Gauss.ASM/Gauss.ASM.asm
+++ Gauss.ASM/Gauss.ASM.asm
@@ -3,6 +3,10 @@
 .model flat, stdcall 
 .xmm
 .data
+
+rowPadded  dd ?
+gaussMask  dd 25 dup(0)
+
 .code
 
 PARAMS STRUCT
@@ -16,7 +20,98 @@ PARAMS STRUCT
 	imgPtr        BYTE PTR  ?
 PARAMS ENDS
 
+; Computes specified pascal triangle row (max 24)
+ComputePascalRow proc x:DWORD
+	local counter:DWORD
+
+; Setting iterator to the initial value
+mov     counter, 1
+
+; Setting the first element
+mov     gaussMask, 1
+
+jmp @startOfFirstLoop
+@firstGaussIteration:
+; Checking iterate conditions
+mov     eax, counter
+inc     eax
+mov     counter, eax
+mov     eax, x
+
+;;;;;;;;;;;;;;;;;;
+;;; First loop ;;;
+@startOfFirstLoop:
+cdq
+sub     eax, edx
+sar     eax, 1
+cmp     counter, eax
+jg      @startOfSecondLoop
+
+; n - i + 1
+mov     eax, x
+sub     eax, counter
+inc     eax
+
+; row[i - 1] * (n - i + 1)
+mov     ecx, counter
+imul    eax, gaussMask [ecx*4-4]
+
+; row[i - 1] * (n - i + 1) / i
+cdq
+idiv    counter
+
+; row[i] = row[i - 1] * (n - i + 1) / i;
+mov     gaussMask [ecx*4], eax
+
+jmp @firstGaussIteration
+
+;;;;;;;;;;;;;;;;;;;
+;;; Second loop ;;;
+@secondGaussIteration:
+
+mov     eax, counter
+inc     eax
+mov     counter, eax
+
+@startOfSecondLoop:
+mov     eax, x
+cmp     counter, eax
+jge     @endOfSecondGaussIteration
+
+; row[i] = row[n - i];
+sub     eax, counter ; n-i
+dec     eax
+mov     ecx, gaussMask [eax*4]
+mov     eax, counter
+mov     gaussMask [eax*4], ecx
+
+jmp @secondGaussIteration
+
+@endOfSecondGaussIteration:
+ret
+
+ComputePascalRow endp
+
+;;;;;;;;;;;;
+;;; Main ;;;
 ComputeGaussBlur proc args:PARAMS
+
+; Compute rowPadded
+mov     eax, args.imgWidth
+imul    eax, 3
+add     eax, 3
+and     eax, 0FFFFFFFCh
+mov     rowPadded, eax
+
+; Compute half of gauss mask
+mov     eax, args.maskSize
+cdq
+sub     eax, edx
+sar     eax, 1
+
+; Compute Pascal row
+invoke  ComputePascalRow, args.maskSize
+invoke  ComputePascalRow, args.maskSize
 
 ret
 
